eea65ec1761f04c8dda3c9711c5ed7225f1523f8
Mozammel Haque <mzmlhaque61@gmail.com>
2015-11-10 03:00:57 +0600

    Fixed all issues

--- signup.php
+++ signup.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-            <form role="form" action="process.php" method="post">
-                <h2>Please Sign Up <small>It's free and always will be.</small></h2>
+            <form action="process.php" method="post" enctype="multipart/form-data">
+                <h2>Please Sign Up <small>List you name with the wanted Criminals</small></h2>
                 <hr class="colorgraph">
                 <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-6">
@@ -31,19 +31,16 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-6">
                         <div class="form-group">
+                            <span>Password</span>
                             <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="6" required>
                         </div>
                     </div>
-<!--                    <div class="col-xs-12 col-sm-6 col-md-6">-->
-<!--                        <div class="form-group">-->
-<!--                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="7" required>-->
-<!--                        </div>-->
-<!--                    </div>-->
                 </div>
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="form-group">
-                            <input class="form-control" type="date" name="dob" placeholder="Date of Birth" required>
+                            <span>Date of Birth</span>
+                            <input class="form-control" type="date" id="datepicker" name="dob" required>
                         </div>
                     </div>
                 </div>
@@ -51,26 +48,25 @@
                     <div class="col-xs-12">
                         <div class="form-group">
                             <p>Profile Picture(300x300)</p>
-                            <input type="file" name="picture" accept="image/*" required>
+                            <input type="file" name="image" required>
                         </div>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-xs-4 col-sm-3 col-md-3">
-					<span class="button-checkbox">
-						<button type="button" class="btn" data-color="info" tabindex="7">Agreement</button>
-                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
-					</span>
+                        <span class="button-checkbox">
+                            <button type="button" class="btn" data-color="info" tabindex="7">Agreement</button>
+                            <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
+                        </span>
                     </div>
                     <div class="col-xs-8 col-sm-9 col-md-9">
                         By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="#" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
                     </div>
                 </div>
-
                 <hr class="colorgraph">
                 <div class="row">
-                    <div class="col-xs-12 col-md-6"><input type="submit" value="register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+                    <div class="col-xs-12 col-md-6"> <input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg"></div>
                     <div class="col-xs-12 col-md-6"><a href="login.php" class="btn btn-success btn-block btn-lg">Sign In</a></div>
                 </div>
             </form>
@@ -78,12 +74,6 @@
     </div>
 </div>
 
-
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-<script scr="script.js"></script>
-</body>
-</html>
+<?php
+    include_once('inc/footer.php');
+?>
