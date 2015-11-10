653b4b58d71bf55856217b6bd0ffb893c18b3f6f
Mozammel Haque <mzmlhaque61@gmail.com>
2015-11-10 11:26:45 +0600

    included image validation

--- process.php
+++ process.php
@@ -8,17 +8,24 @@
 include_once('inc/header.php');
 if(isset($_POST['submit'])){
     $file = $_FILES['image'];
+    $image_name = $file['name'];
+    $image_temp = $file['tmp_name'];
+    $image_type = $file['type'];
+    $image_size = $file['size'];
+    $image_error = $file['error'];
+    $accept = array('jpg','jpeg','png');
+    $temp = explode(".", $image_name);
     if($file['size']>=50000){
         echo("<script>alert('Image size is not allowed')</script>");
         echo("<script>location.href='signup.php'</script>");
-    }else
+    }elseif(!in_array(end($temp), $accept))
     {
-        $image_name = $file['name'];
-        $image_temp = $file['tmp_name'];
-        $image_type = $file['type'];
-        $image_size = $file['size'];
-        $image_error = $file['error'];
-        $temp = explode(".", $image_name);
+        echo("<script>alert('please upload jpg or png file')</script>");
+        echo("<script>location.href='signup.php'</script>");
+    }
+
+    else
+    {
         $newfilename = round(microtime(true)) . '.' . end($temp);
         move_uploaded_file($image_temp,"images/$newfilename");
         echo "<img src=\"images/$newfilename\">";
@@ -38,7 +45,7 @@ if(isset($_POST['submit'])){
         $db_pass =$databse_pass;
         $db_name=$database_name;
         $dbcon=$connection_object->connection('localhost',$db_user,$db_pass,$db_name);
-        $sql = "INSERT INTO `datadriven`.`user` (`id`, `fname`, `lname`, `email`, `address`, `crimes`, `password`, `dob`, `picture`, `rank`) VALUES (NULL, '$fname', '$lname', '$user', '$address', '$crimes', '$password', '$dob', '$picture', '$rank');";
+        $sql = "INSERT INTO `user` (`id`, `fname`, `lname`, `email`, `address`, `crimes`, `password`, `dob`, `picture`, `rank`) VALUES (NULL, '$fname', '$lname', '$user', '$address', '$crimes', '$password', '$dob', '$picture', '$rank');";
         $preparestatement=$dbcon->prepare($sql);
         $preparestatement->execute();
         echo("<script>alert('Registered Successfully. Please log in with your password...')</script>");
